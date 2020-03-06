fa1bf2a97e6cc85a21de7cc7c5d81b96cd56d3f3
OzanGUVEN2020 <61184104+OzanGUVEN2020@users.noreply.github.com>
2020-03-06 15:11:18 +0100

    Fin de CartesianCoordinates

---
+++ ProjetJavaStar/src/ch/epfl/rigel/coordinates/CartesianCoordinates.java
@@ -0,0 +1,64 @@
+package ch.epfl.rigel.coordinates;
+
+import java.util.Locale;
+
+/**
+ * Cartesian coordinates system
+ *
+ * @author Ozan Güven (297076)
+ */
+public final class CartesianCoordinates {
+    private double x, y;
+
+    private CartesianCoordinates(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * Creates cartesian coordinates with given x and y values
+     *
+     * @param x the abscissa coordinate
+     * @param y the ordinate coordinate
+     * @return the cartesian coordinates for the given values
+     */
+    public static CartesianCoordinates of(double x, double y) {
+        return new CartesianCoordinates(x, y);
+    }
+
+    /**
+     * Returns the x coordinate (abscissa)
+     *
+     * @return the x coordinate
+     */
+    public double x() {
+        return x;
+    }
+
+    /**
+     * Returns the y coordinate (ordinate)
+     *
+     * @return the y coordinate
+     */
+    public double y() {
+        return y;
+    }
+
+    @Override
+    public int hashCode() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String toString() {
+        return String.format(Locale.ROOT,
+                "(x=%.4f, y=%.4f)",
+                x,
+                y);
+    }
+}
