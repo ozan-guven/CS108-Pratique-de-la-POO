2199e3f56413ea10f78a5c0f2602961c13ef6852
OzanGUVEN2020 <61184104+OzanGUVEN2020@users.noreply.github.com>
2020-03-15 12:28:17 +0100

    Fin de CelestialObjectModel

---
+++ ProjetJavaStar/src/ch/epfl/rigel/astronomy/CelestialObjectModel.java
@@ -0,0 +1,26 @@
+package ch.epfl.rigel.astronomy;
+
+import ch.epfl.rigel.coordinates.EclipticToEquatorialConversion;
+
+/**
+ * Interface representing a celestial object model,
+ * tools to compute characteristics of an object at a given time.
+ *
+ * @param <O> type of the object modeled by the model
+ * @author Ozan Güven (297076)
+ */
+public interface CelestialObjectModel<O> {
+
+    /**
+     * Returns the modeled object for the given number of days after epoch
+     * J2010 (may be negative) and, using the given coordinate conversion
+     * system, computes its equatorial coordinates given its ecliptic
+     * coordinates.
+     *
+     * @param daysSinceJ2010                 the number of days since J2010
+     * @param eclipticToEquatorialConversion the conversion that will be used
+     * @return the modeled object
+     */
+    public abstract O at(double daysSinceJ2010, EclipticToEquatorialConversion eclipticToEquatorialConversion);
+
+}
