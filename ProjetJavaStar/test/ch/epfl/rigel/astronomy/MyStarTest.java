c099dc69489c8c97ae59d82f2cbf66392722c900
OzanGUVEN2020 <61184104+OzanGUVEN2020@users.noreply.github.com>
2020-03-24 10:31:16 +0100

    Masse tests pour Star

--- ProjetJavaStar/test/ch/epfl/rigel/astronomy/MyStarTest.java
+++ ProjetJavaStar/test/ch/epfl/rigel/astronomy/MyStarTest.java
@@ -8,9 +8,58 @@ import static org.junit.jupiter.api.Assertions.*;
 class MyStarTest {
 
     @Test
-    void hipparcosId() {
+    void constructorThrowsExceptionsWhenNeeded() {
+        assertThrows(NullPointerException.class, () -> {
+            Star star = new Star(-5, null,
+                    EquatorialCoordinates.of(0, 0), -10, (float) 1);
+        });
 
+        assertThrows(NullPointerException.class, () -> {
+            Star star = new Star(-5, "Beluga",
+                    null, -10, (float) 1);
+        });
     }
+
+    @Test
+    void hipparcosIdThrowsExceptions() {
+        //hippardcosId negative
+        assertThrows(IllegalArgumentException.class, () -> {
+            Star star = new Star(-5, "Beluga",
+                    EquatorialCoordinates.of(0, 0), -10, (float) -0.1);
+        });
+
+        //colorIndex pas dans [-0.5, 5.5]
+        assertThrows(IllegalArgumentException.class, () -> {
+            Star star = new Star(0, "Beluga",
+                    EquatorialCoordinates.of(0, 0), -10, (float) -0.5001);
+        });
+
+        //colorIndex pas dans [-0.5, 5.5]
+        assertThrows(IllegalArgumentException.class, () -> {
+            Star star = new Star(1, "Beluga",
+                    EquatorialCoordinates.of(0, 0), -10, (float) 5.50001);
+        });
+
+        //Checks if no exceptions are thrown
+        assertDoesNotThrow(() -> {
+            Star star = new Star(0, "Beluga",
+                    EquatorialCoordinates.of(0, 0), -10, (float) -0.500);
+        });
+
+        //Checks if no exceptions are thrown
+        assertDoesNotThrow(() -> {
+            Star star = new Star(1, "Beluga",
+                    EquatorialCoordinates.of(0, 0), -10, (float) 5.500);
+        });
+
+        //Checks if no exceptions are thrown
+        assertDoesNotThrow(() -> {
+            Star star = new Star(1, "Beluga",
+                    EquatorialCoordinates.of(0, 0), -10, (float) 5.25);
+        });
+    }
+
+    //*************************** TEST DE LA CLASSE ****************************************
 
     @Test
     void colorTemperature() {
