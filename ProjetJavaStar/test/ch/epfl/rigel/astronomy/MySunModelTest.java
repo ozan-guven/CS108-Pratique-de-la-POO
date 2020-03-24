17a7aceccd8f95587c346c6814a2b1deb181f4ed
OzanGUVEN2020 <61184104+OzanGUVEN2020@users.noreply.github.com>
2020-03-24 10:02:30 +0100

    Tests Star et SunModel

---
+++ ProjetJavaStar/test/ch/epfl/rigel/astronomy/MySunModelTest.java
@@ -0,0 +1,43 @@
+package ch.epfl.rigel.astronomy;
+
+import ch.epfl.rigel.coordinates.EclipticToEquatorialConversion;
+import org.junit.jupiter.api.Test;
+
+import java.time.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MySunModelTest {
+
+    //******************** TESTS DE LA CLASSE *******************
+
+    @Test
+    void at() {
+        assertEquals(5.9325494700300885,
+                SunModel.SUN.at(27 + 31,
+                        new EclipticToEquatorialConversion(
+                                ZonedDateTime.of(
+                                        LocalDate.of(2010,  Month.FEBRUARY, 27),
+                                        LocalTime.of(0,0),
+                                        ZoneOffset.UTC)))
+                .equatorialPos().ra());
+
+        assertEquals(8.3926828082978,
+                SunModel.SUN.at(-2349,
+                        new EclipticToEquatorialConversion(
+                                ZonedDateTime.of(
+                                        LocalDate.of(2003, Month.JULY, 27),
+                                        LocalTime.of(0, 0, 0, 0),
+                                        ZoneOffset.UTC)))
+                        .equatorialPos().raHr());
+
+        assertEquals(19.35288373097352,
+                SunModel.SUN.at(-2349,
+                        new EclipticToEquatorialConversion(
+                                ZonedDateTime.of(
+                                        LocalDate.of(2003, Month.JULY, 27),
+                                        LocalTime.of(0, 0, 0, 0),
+                                        ZoneOffset.UTC)))
+                        .equatorialPos().decDeg());
+    }
+}
