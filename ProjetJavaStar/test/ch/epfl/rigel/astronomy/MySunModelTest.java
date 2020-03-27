73ffb58193c2abb940498b1ed1fae89bedf1f7b6
OzanGUVEN2020 <61184104+OzanGUVEN2020@users.noreply.github.com>
2020-03-27 12:41:24 +0100

    Modification et ajout de test MySunModelTest

--- ProjetJavaStar/test/ch/epfl/rigel/astronomy/MySunModelTest.java
+++ ProjetJavaStar/test/ch/epfl/rigel/astronomy/MySunModelTest.java
@@ -48,10 +48,18 @@ class MySunModelTest {
 
         double daySinceJ2010 = Epoch.J2010.daysUntil(date);
 
-        assertEquals((float) Angle.ofDMS(0, 31, 30), SunModel.SUN.at(daySinceJ2010,
+        assertEquals(Angle.ofDMS(0, 31, 30), (float)  SunModel.SUN.at(daySinceJ2010,
                 new EclipticToEquatorialConversion(date)).angularSize());
 
-        assertEquals((float) 3.528210, Angle.normalizePositive(SunModel.SUN.at(daySinceJ2010,
+        assertEquals(3.528210, (float)  Angle.normalizePositive(SunModel.SUN.at(daySinceJ2010,
                 new EclipticToEquatorialConversion(date)).meanAnomaly()));
+
+        ZonedDateTime date1 = ZonedDateTime.of(
+                LocalDate.of(2003, Month.JULY, 27),
+                LocalTime.of(0, 0),
+                ZoneOffset.UTC);
+
+        assertEquals(Angle.ofDeg(201.159131), (float) Angle.normalizePositive(SunModel.SUN.at(-2349,
+                new EclipticToEquatorialConversion(date1)).meanAnomaly()));
     }
 }
