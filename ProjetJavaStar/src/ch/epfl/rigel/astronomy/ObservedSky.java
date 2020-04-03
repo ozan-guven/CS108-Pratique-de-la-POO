07f96588e9fe44ec906cab1b24c0d5dd01a03eb2
OzanGUVEN2020 <61184104+OzanGUVEN2020@users.noreply.github.com>
2020-04-03 20:04:01 +0200

    Création de ObservedSky

---
+++ ProjetJavaStar/src/ch/epfl/rigel/astronomy/ObservedSky.java
@@ -0,0 +1,29 @@
+package ch.epfl.rigel.astronomy;
+
+import ch.epfl.rigel.coordinates.GeographicCoordinates;
+import ch.epfl.rigel.coordinates.StereographicProjection;
+
+import java.time.ZonedDateTime;
+
+/**
+ * Class representing a set of celestial objects projected
+ * onto a plan with a stereographic projection
+ *
+ * @author Robin Goumaz (301420)
+ * @author Ozan Güven (297076)
+ */
+public final class ObservedSky {
+
+    private final ZonedDateTime time;
+    private final GeographicCoordinates coordinates;
+    private final StereographicProjection projection;
+    private final StarCatalogue catalogue;
+
+    public ObservedSky(ZonedDateTime time, GeographicCoordinates coordinates,
+                       StereographicProjection projection, StarCatalogue catalogue) {
+        this.time = time;
+        this.coordinates = coordinates;
+        this.projection = projection;
+        this.catalogue = catalogue;
+    }
+}
