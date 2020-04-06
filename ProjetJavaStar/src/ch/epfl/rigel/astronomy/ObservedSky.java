242bee0a00372a17d6a1a8b6a2d1986926c1599c
OzanGUVEN2020 <61184104+OzanGUVEN2020@users.noreply.github.com>
2020-04-06 21:33:11 +0200

    ObservedSky

--- ProjetJavaStar/src/ch/epfl/rigel/astronomy/ObservedSky.java
+++ ProjetJavaStar/src/ch/epfl/rigel/astronomy/ObservedSky.java
@@ -1,9 +1,6 @@
 package ch.epfl.rigel.astronomy;
 
-import ch.epfl.rigel.coordinates.CartesianCoordinates;
-import ch.epfl.rigel.coordinates.EclipticToEquatorialConversion;
-import ch.epfl.rigel.coordinates.GeographicCoordinates;
-import ch.epfl.rigel.coordinates.StereographicProjection;
+import ch.epfl.rigel.coordinates.*;
 
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
@@ -22,12 +19,13 @@ public final class ObservedSky {
     private final StereographicProjection projection;
     private final StarCatalogue catalogue;
 
-    private Sun sun;
-    private Moon moon;
-    private ArrayList<Planet> planets;
+    private final Sun sun;
+    private final Moon moon;
+    private final ArrayList<Planet> planets;
     private ArrayList<Star> stars;
 
-    private CartesianCoordinates sunCoordinates;
+    private final CartesianCoordinates sunCoordinates;
+    private final CartesianCoordinates moonCoordinates;
 
     public ObservedSky(ZonedDateTime time, GeographicCoordinates coordinates,
                        StereographicProjection projection, StarCatalogue catalogue) {
@@ -37,18 +35,24 @@ public final class ObservedSky {
         this.catalogue = catalogue;
 
         double daysUntilJ2010 = Epoch.J2010.daysUntil(time);
-        EclipticToEquatorialConversion conversion = new EclipticToEquatorialConversion(time);
+        EclipticToEquatorialConversion conversionToEqu = new EclipticToEquatorialConversion(time);
 
-        sun = SunModel.SUN.at(daysUntilJ2010, conversion);
-        moon = MoonModel.MOON.at(daysUntilJ2010, conversion);
+        sun = SunModel.SUN.at(daysUntilJ2010, conversionToEqu);
+        moon = MoonModel.MOON.at(daysUntilJ2010, conversionToEqu);
 
         planets = new ArrayList<>();
         for (PlanetModel planet : PlanetModel.values()) {
             if (planet != PlanetModel.EARTH) {
-                planets.add(planet.at(daysUntilJ2010, conversion));
+                planets.add(planet.at(daysUntilJ2010, conversionToEqu));
             }
         }
+
         //TODO : Et comment on sait la position des étoiles ?
+
+        EquatorialToHorizontalConversion conversionToHor = new EquatorialToHorizontalConversion(time, coordinates);
+
+        sunCoordinates = projection.apply(conversionToHor.apply(sun.equatorialPos()));
+        moonCoordinates = projection.apply(conversionToHor.apply(moon.equatorialPos()));
     }
 
     public Sun sun() {
