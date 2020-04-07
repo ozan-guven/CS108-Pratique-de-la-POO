42edc7b9f7abf3c4b3926985b53291576f555943
OzanGUVEN2020 <61184104+OzanGUVEN2020@users.noreply.github.com>
2020-04-07 20:51:16 +0200

    Création de BlackBodyColorTest

---
+++ ProjetJavaStar/test/ch/epfl/rigel/gui/BlackBodyColorTest.java
@@ -0,0 +1,19 @@
+package ch.epfl.rigel.gui;
+
+import javafx.scene.paint.Color;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BlackBodyColorTest {
+
+    @Test
+    void colorForTemperature() {
+
+        //**************************** TESTS DE LA CLASSE ****************************
+        assertEquals(Color.web("#ffcc99"), BlackBodyColor.colorForTemperature(3798.1409));
+        assertEquals(Color.web("#c8d9ff"), BlackBodyColor.colorForTemperature(10500d));
+
+        assertThrows(IllegalArgumentException.class, () -> BlackBodyColor.colorForTemperature(40_000.00001d));
+    }
+}
