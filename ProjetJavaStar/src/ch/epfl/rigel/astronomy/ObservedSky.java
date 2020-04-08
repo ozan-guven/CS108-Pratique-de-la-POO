91407dffbc024eafb9dfdb967819e5afcc6b2bcf
OzanGUVEN2020 <61184104+OzanGUVEN2020@users.noreply.github.com>
2020-04-08 21:47:29 +0200

    ObservedSky

--- ProjetJavaStar/src/ch/epfl/rigel/astronomy/ObservedSky.java
+++ ProjetJavaStar/src/ch/epfl/rigel/astronomy/ObservedSky.java
@@ -1,6 +1,7 @@
 package ch.epfl.rigel.astronomy;
 
 import ch.epfl.rigel.coordinates.*;
+import ch.epfl.rigel.math.ClosedInterval;
 
 import java.time.ZonedDateTime;
 import java.util.*;
@@ -28,6 +29,8 @@ public final class ObservedSky {
     private final CartesianCoordinates moonCoordinates;
     private final double[] planetPositions = new double[14];
 
+    private Map<CartesianCoordinates, CelestialObject> mapOfAll;
+
     public ObservedSky(ZonedDateTime time, GeographicCoordinates coordinates,
                        StereographicProjection projection, StarCatalogue catalogue) {
         this.time = time;
@@ -39,8 +42,16 @@ public final class ObservedSky {
         EclipticToEquatorialConversion conversionToEqu = new EclipticToEquatorialConversion(time);
         EquatorialToHorizontalConversion conversionToHor = new EquatorialToHorizontalConversion(time, coordinates);
 
+        mapOfAll = new HashMap<>();
+
         sun = SunModel.SUN.at(daysUntilJ2010, conversionToEqu);
         moon = MoonModel.MOON.at(daysUntilJ2010, conversionToEqu);
+
+        sunCoordinates = projection.apply(conversionToHor.apply(sun.equatorialPos()));
+        moonCoordinates = projection.apply(conversionToHor.apply(moon.equatorialPos()));
+
+        mapOfAll.put(sunCoordinates, sun);
+        mapOfAll.put(moonCoordinates, moon);
 
         planets = new ArrayList<>();
         //List<Double> planetCoordinates = new ArrayList<>();
@@ -52,14 +63,13 @@ public final class ObservedSky {
 
                 CartesianCoordinates planetProjection = projection.apply(conversionToHor.apply(newPlanet.equatorialPos()));
                 planetPositions[i++] = planetProjection.x();
-                planetPositions[i++] = planetProjection.y(); //TODO : Ca va pas marcher faut revoir !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                planetPositions[i++] = planetProjection.y();
+
+                mapOfAll.put(planetProjection, newPlanet);
             }
         }
 
         //TODO : Et comment on sait la position des étoiles ??????????????????????????????
-
-        sunCoordinates = projection.apply(conversionToHor.apply(sun.equatorialPos()));
-        moonCoordinates = projection.apply(conversionToHor.apply(moon.equatorialPos()));
     }
 
     public Sun sun() {
@@ -103,6 +113,19 @@ public final class ObservedSky {
     }
 
     public CelestialObject objectClosestTo(CartesianCoordinates coordinates, double maxDistance) {
-        return null;
+        CartesianCoordinates coordOfNearest = null;
+        double currentDistance;
+        ClosedInterval intervalX = ClosedInterval.of(coordinates.x() - maxDistance, coordinates.x() + maxDistance);
+        ClosedInterval intervalY = ClosedInterval.of(coordinates.y() - maxDistance, coordinates.y() + maxDistance);
+        for (CartesianCoordinates coord : mapOfAll.keySet()) {
+            if (intervalX.contains(coord.x()) && intervalY.contains(coord.y())) {
+                //coordOfNearest = currentDistance > distance(coordinates, coord) ? coord : coordOfNearest;
+            }
+        }
+        return mapOfAll.get(coordOfNearest);
+    }
+
+    private double distance(CartesianCoordinates coord1, CartesianCoordinates coord2) {
+        return Math.sqrt(Math.pow(coord1.x() - coord2.x(), 2) + Math.pow(coord1.y() - coord2.y(), 2));
     }
 }
