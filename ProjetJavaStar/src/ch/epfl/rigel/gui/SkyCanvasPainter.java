bc61db01b04ff77bd914e4a3bc40ebfcbf6be2f2
OzanGUVEN2020 <61184104+OzanGUVEN2020@users.noreply.github.com>
2020-04-10 21:12:34 +0200

    Début de SkyCanvasPainter

---
+++ ProjetJavaStar/src/ch/epfl/rigel/gui/SkyCanvasPainter.java
@@ -0,0 +1,50 @@
+package ch.epfl.rigel.gui;
+
+import ch.epfl.rigel.astronomy.CelestialObject;
+import ch.epfl.rigel.astronomy.ObservedSky;
+import ch.epfl.rigel.astronomy.Star;
+import ch.epfl.rigel.coordinates.StereographicProjection;
+import ch.epfl.rigel.math.Angle;
+import ch.epfl.rigel.math.ClosedInterval;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.transform.Transform;
+
+/**
+ * Class used to draw the sky on the screen
+ *
+ * @author Robin Goumaz (301420)
+ * @author Ozan Güven (297076)
+ */
+public class SkyCanvasPainter {
+
+    private Canvas canvas;
+    private GraphicsContext ctx;
+
+    public SkyCanvasPainter(Canvas canvas) {
+        this.canvas = canvas;
+        ctx = canvas.getGraphicsContext2D();
+    }
+
+    public void clear() {
+        ctx.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+    }
+
+    public void drawStars(ObservedSky sky, StereographicProjection projection, Transform planeToCanvas) {
+        double[] transformed = new double[sky.starPositions().length];
+        planeToCanvas.transform2DPoints(sky.starPositions(), 0, transformed, 0, sky.starPositions().length);
+
+        int i = 0;
+        for (Star star : sky.stars()) {
+            ctx.setFill(BlackBodyColor.colorForTemperature(star.colorTemperature()));
+            ctx.fillOval(transformed[i++], transformed[i++], diameterFromMagnitude(star), diameterFromMagnitude(star));
+        }
+    }
+
+    private double diameterFromMagnitude(CelestialObject celestialObject) {
+        ClosedInterval interval = ClosedInterval.of(-2, 5);
+        double clippedMagnitude = interval.clip(celestialObject.magnitude());
+        double factor = (99 - 17 * clippedMagnitude) / 140;
+        return factor * 2 * Math.tan(Angle.ofDeg(0.5) / 4);
+    }
+}
