de5b682476ada5f6385d0bc8da5bc198cb597e65
OzanGUVEN2020 <61184104+OzanGUVEN2020@users.noreply.github.com>
2020-04-11 17:03:39 +0200

    drawStars fini

--- ProjetJavaStar/src/ch/epfl/rigel/gui/SkyCanvasPainter.java
+++ ProjetJavaStar/src/ch/epfl/rigel/gui/SkyCanvasPainter.java
@@ -24,31 +24,52 @@ public class SkyCanvasPainter {
     private Canvas canvas;
     private GraphicsContext ctx;
 
+    /**
+     * Initializes the sky painter
+     *
+     * @param canvas the canvas to draw onto
+     */
     public SkyCanvasPainter(Canvas canvas) {
         this.canvas = canvas;
         ctx = canvas.getGraphicsContext2D();
     }
 
+    /**
+     * Clears the canvas and sets its color to black
+     */
     public void clear() {
         ctx.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
         ctx.setFill(Color.BLACK);
         ctx.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
     }
 
+    /**
+     * Draws the asterisms and the stars onto the canvas
+     *
+     * @param sky           the observed sky
+     * @param projection    the projection used
+     * @param planeToCanvas the affine transformation to transform from
+     *                      cartesian coordinates to the coordinates of the sreen
+     */
     public void drawStars(ObservedSky sky, StereographicProjection projection, Transform planeToCanvas) {
         double[] transformed = new double[sky.starPositions().length];
         planeToCanvas.transform2DPoints(sky.starPositions(), 0, transformed, 0, sky.starPositions().length / 2);
 
         ctx.setLineWidth(1);
         ctx.setStroke(Color.BLUE);
-
         List<Integer> asterismIndices;
+        int currentStar, nextStar;
         for (Asterism asterism : sky.asterisms()) {
             asterismIndices = sky.asterismIndices(asterism);
             ctx.beginPath();
             for (int i = 0; i < asterismIndices.size() - 1; i++) {
-                ctx.moveTo(transformed[2 * asterismIndices.get(i)], transformed[2 * asterismIndices.get(i) + 1]);
-                ctx.lineTo(transformed[2 * asterismIndices.get(i + 1)], transformed[2 * asterismIndices.get(i + 1) + 1]);
+                currentStar = 2 * asterismIndices.get(i);
+                nextStar = 2 * asterismIndices.get(i + 1);
+                if (canvas.getBoundsInLocal().contains(transformed[currentStar], transformed[currentStar + 1])
+                        || canvas.getBoundsInLocal().contains(transformed[nextStar], transformed[nextStar + 1])) { //Checks if there is a path outside the canvas
+                    ctx.moveTo(transformed[currentStar], transformed[currentStar + 1]);
+                    ctx.lineTo(transformed[nextStar], transformed[nextStar + 1]);
+                }
             }
             ctx.closePath();
             ctx.stroke();
