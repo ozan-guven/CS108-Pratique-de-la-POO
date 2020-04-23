03241fe890febf60a764b7e60b452e7a6d293f43
OzanGUVEN2020 <61184104+OzanGUVEN2020@users.noreply.github.com>
2020-04-23 21:51:02 +0200

    TimeAccelerator

--- ProjetJavaStar/src/ch/epfl/rigel/gui/TimeAccelerator.java
+++ ProjetJavaStar/src/ch/epfl/rigel/gui/TimeAccelerator.java
@@ -1,7 +1,15 @@
 package ch.epfl.rigel.gui;
 
+import java.time.Duration;
 import java.time.ZonedDateTime;
+import java.time.temporal.ChronoUnit;
 
+/**
+ * Interface representing a time accelerator
+ *
+ * @author Robin Goumaz (301420)
+ * @author Ozan Güven (297076)
+ */
 @FunctionalInterface
 public interface TimeAccelerator {
 
@@ -15,5 +23,27 @@ public interface TimeAccelerator {
      */
     ZonedDateTime adjust(ZonedDateTime initialTime, long timeSinceAnimation);
 
+    /**
+     * Returns a continuous accelerator with respect to the
+     * given acceleration factor
+     *
+     * @param accFactor the acceleration factor
+     * @return the continuous accelerator
+     */
+    static TimeAccelerator continuous(int accFactor) {
+        return (initialTime, timeSinceAnimation) -> initialTime.plusNanos(accFactor * timeSinceAnimation);
+    }
 
+    /**
+     * Returns a discrete accelerator with respect to the
+     * given advancement frequency and the step
+     *
+     * @param advFreq the advancement frequency
+     * @param step the step
+     * @return the discrete accelerator
+     */
+    static TimeAccelerator discrete(int advFreq, Duration step) {
+        return (initialTime, timeSinceAnimation) ->
+                initialTime.plusNanos(Math.abs(advFreq * timeSinceAnimation) * step.get(ChronoUnit.NANOS));
+    }
 }
