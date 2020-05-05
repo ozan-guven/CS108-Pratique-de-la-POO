a1b50cecbdc030506c2db5c7b1050403e422f316
OzanGUVEN2020 <61184104+OzanGUVEN2020@users.noreply.github.com>
2020-05-05 10:39:31 +0200

    ObserverLocationBean et ViewingParametersBean

---
+++ ProjetJavaStar/src/ch/epfl/rigel/gui/ObserverLocationBean.java
@@ -0,0 +1,67 @@
+package ch.epfl.rigel.gui;
+
+import ch.epfl.rigel.coordinates.GeographicCoordinates;
+import javafx.beans.binding.Bindings;
+import javafx.beans.binding.ObjectBinding;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+
+/**
+ * A bean for the location of the observer
+ *
+ * @author Robin Goumaz (301420)
+ * @author Ozan Güven (297076)
+ */
+public class ObserverLocationBean {
+    private final DoubleProperty lonDeg = new SimpleDoubleProperty();
+    private final DoubleProperty latDeg = new SimpleDoubleProperty();
+    private final ObjectBinding<GeographicCoordinates> coordinates;
+
+    public ObserverLocationBean() {
+        coordinates = Bindings.createObjectBinding(() ->
+                GeographicCoordinates.ofDeg(lonDeg.get(), latDeg.get()), lonDeg, latDeg);
+    }
+
+    public double getLonDeg() {
+        return lonDeg.get();
+    }
+
+    public DoubleProperty lonDegProperty() {
+        return lonDeg;
+    }
+
+    public void setLonDeg(double lonDeg) {
+        this.lonDeg.set(lonDeg);
+    }
+
+    public double getLatDeg() {
+        return latDeg.get();
+    }
+
+    public DoubleProperty latDegProperty() {
+        return latDeg;
+    }
+
+    public void setLatDeg(double latDeg) {
+        this.latDeg.set(latDeg);
+    }
+
+    public GeographicCoordinates getCoordinates() {
+        return coordinates.get();
+    }
+
+    public ObjectBinding<GeographicCoordinates> coordinatesProperty() {
+        return coordinates;
+    }
+}
+
+/*//TODO TEST
+class ObserverTest {
+    public static void main(String[] args) {
+        ObserverLocationBean bean = new ObserverLocationBean();
+        bean.setLatDeg(20);
+        System.out.println(bean.getCoordinates());
+        bean.setLonDeg(35);
+        System.out.println(bean.getCoordinates());
+    }
+}*/
---
+++ ProjetJavaStar/src/ch/epfl/rigel/gui/ViewingParametersBean.java
@@ -0,0 +1,51 @@
+package ch.epfl.rigel.gui;
+
+import ch.epfl.rigel.coordinates.HorizontalCoordinates;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleObjectProperty;
+
+/**
+ * A bean for the viewing parameters like :
+ * <ul>
+ *     <li>the field of view</li>
+ *     <li>the center of the projection</li>
+ * </ul>
+ *
+ * @author Robin Goumaz (301420)
+ * @author Ozan Güven (297076)
+ */
+public class ViewingParametersBean {
+    private final DoubleProperty fieldOfViewDeg = new SimpleDoubleProperty();
+    private final ObjectProperty<HorizontalCoordinates> center = new SimpleObjectProperty<>();
+
+    public ViewingParametersBean() {
+        fieldOfViewDeg.set(0);
+        center.set(null);
+    }
+
+    public double getFieldOfViewDeg() {
+        return fieldOfViewDeg.get();
+    }
+
+    public DoubleProperty fieldOfViewDegProperty() {
+        return fieldOfViewDeg;
+    }
+
+    public void setFieldOfViewDeg(double fieldOfViewDeg) {
+        this.fieldOfViewDeg.set(fieldOfViewDeg);
+    }
+
+    public HorizontalCoordinates getCenter() {
+        return center.get();
+    }
+
+    public ObjectProperty<HorizontalCoordinates> centerProperty() {
+        return center;
+    }
+
+    public void setCenter(HorizontalCoordinates center) {
+        this.center.set(center);
+    }
+}
