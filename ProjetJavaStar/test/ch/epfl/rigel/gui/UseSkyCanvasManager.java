f79122cdbb9e6aedf536673346a5eeb219fd7e0e
OzanGUVEN2020 <61184104+OzanGUVEN2020@users.noreply.github.com>
2020-05-06 09:14:26 +0200

    Changement de la coordonnée de départ car suivantl'implémentation ça bug

--- ProjetJavaStar/test/ch/epfl/rigel/gui/UseSkyCanvasManager.java
+++ ProjetJavaStar/test/ch/epfl/rigel/gui/UseSkyCanvasManager.java
@@ -43,7 +43,7 @@ public final class UseSkyCanvasManager extends Application {
             ViewingParametersBean viewingParametersBean =
                     new ViewingParametersBean();
             viewingParametersBean.setCenter(
-                    HorizontalCoordinates.ofDeg(180, 42));
+                    HorizontalCoordinates.ofDeg(180.000000000001, 42));
             viewingParametersBean.setFieldOfViewDeg(70);
 
             SkyCanvasManager canvasManager = new SkyCanvasManager(
